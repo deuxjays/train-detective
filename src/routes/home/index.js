1aca1433c5d20e94a203873642f4e1b269091b8c
James <32144793+j-dav@users.noreply.github.com>
2022-07-01 12:02:01 +0100

    Add preact library test suite and scss load (#6)

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,10 +1,14 @@
 import { h } from 'preact';
-import style from './style.css';
+import style from './style.scss';
+
+// Components
+import PrimaryButton from '../../components/button';
 
 const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
 		<p>This is the Home component.</p>
+		<PrimaryButton>This is a button</PrimaryButton>
 	</div>
 );
 
